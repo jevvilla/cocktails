3fcb1f23638c5688dd867cf082eba74ee53ae8d8
jesvilla <jvilla@fullstacklabs.co>
2019-10-02 06:46:43 -0500

    requesting all data including details

--- src/actions/cocktails.js
+++ src/actions/cocktails.js
@@ -1,4 +1,5 @@
 import * as types from './types';
+import { BASE_URL } from '../common/constants';
 
 export const fetchingCocktailsRequest = () => ({ type: types.FETCHING_COCKTAILS_REQUEST });
 
@@ -16,13 +17,38 @@ export const fetchCocktails = () => {
   return async dispatch => {
     dispatch(fetchingCocktailsRequest());
     try {
-      const response = await fetch(
-        'https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=Cocktail_glass',
-      );
-      const cocktails = await response.json();
+      const response = await fetch(`${BASE_URL}filter.php?g=Cocktail_glass`);
+      const json = await response.json();
+      const cocktails = await fetchCocktailsDetails(json);
       dispatch(fetchingCocktailsSuccess(cocktails));
     } catch (error) {
       dispatch(fetchingCocktailsFailure(error));
     }
   };
+};
+
+const fetchCocktailsDetails = async cocktails => {
+  const drinks = await Promise.all(
+    cocktails.drinks.map(async cocktail => {
+      try {
+        const detail = await fetchDetails(cocktail.idDrink);
+        cocktail.details = detail.drinks;
+        return cocktail;
+      } catch (error) {
+        return [];
+      }
+    }),
+  );
+
+  return { drinks };
+};
+
+const fetchDetails = async idDrink => {
+  try {
+    const response = await fetch(`${BASE_URL}lookup.php?i=${idDrink}`);
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    return null;
+  }
 };
