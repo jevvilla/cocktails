4727a6ccea05cb26ce3b8d8f9d1dc74c335755da
jesvilla <jvilla@fullstacklabs.co>
2019-10-02 10:54:34 -0500

    getIngredients is implemented in CocktailsCardList

--- src/screens/Overview/components/CocktailsCardList/index.jsx
+++ src/screens/Overview/components/CocktailsCardList/index.jsx
@@ -1,23 +1,39 @@
 import React, { PureComponent } from 'react';
 import { FlatList } from 'react-native';
 import PropTypes from 'prop-types';
+import { take } from 'lodash';
 
 import { navigationShape } from '../../../../common/propTypes';
 import CocktailCard from '../../../../common/components/CocktailCard';
 import * as routes from '../../../../navigation/routes';
+import getFilteredIngredients from '../../../../common/tools';
 
 import styles from './styles';
 
-class Overview extends PureComponent {
+class CocktailsCardList extends PureComponent {
   navigate = item => {
     const { navigation } = this.props;
     navigation.navigate(routes.DETAILS, { headerTitle: item.strDrink });
+  };
+
+  getIngredients = item => {
+    const filteredElements = getFilteredIngredients(item);
+    const ingredients = take(filteredElements, 2);
+    const leftIngredients = filteredElements.length - 2;
+
+    if (leftIngredients > 0) {
+      const message = leftIngredients > 1 ? 'ingredientes' : 'ingrediente';
+      ingredients.push(`y ${leftIngredients} ${message} mas`);
+    }
+
+    return ingredients;
   };
 
   renderItem = ({ item }) => (
     <CocktailCard
       uri={item.strDrinkThumb}
       title={item.strDrink}
+      ingredients={this.getIngredients(item)}
       onPress={() => this.navigate(item)}
     />
   );
@@ -39,9 +55,9 @@ class Overview extends PureComponent {
   }
 }
 
-Overview.propTypes = {
+CocktailsCardList.propTypes = {
   navigation: navigationShape.isRequired,
   data: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
-export default Overview;
+export default CocktailsCardList;
