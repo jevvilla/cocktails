8dece4e492c29b41759f33b7ca02731cb25c2a01
jesvilla <jvilla@fullstacklabs.co>
2019-10-02 18:27:13 -0500

    fixing overflow array

--- src/common/tools.js
+++ src/common/tools.js
@@ -14,13 +14,24 @@ export const getFilteredIngredients = ({ details }) => {
 
 export const getIngredientsAndMeasurements = item => {
   const ingredients = getFilteredIngredients(item);
+  const measurements = getFilteredMeasures(item);
+  const measuresAndIngredients = [];
+
+  for (let i = 0; i < ingredients.length; i += 1) {
+    measuresAndIngredients.push(`${measurements[i].trim()} - ${ingredients[i].trim()}`);
+  }
+
+  return measuresAndIngredients;
+};
+
+export const getFilteredMeasures = item => {
   const keys = Object.keys(item.details[0]);
   const measurements = [];
 
-  for (let i = 0; i <= keys.length; i += 1) {
+  for (let i = 1; i < keys.length; i += 1) {
     const measure = item.details[0][`strMeasure${i}`];
     if (measure) {
-      measurements.push(`${measure} - ${ingredients[i]}`);
+      measurements.push(measure);
     }
   }
 
