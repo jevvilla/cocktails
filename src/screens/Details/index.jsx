01cca5b32f88028207e0fe6f14693084cc206114
jesvilla <jvilla@fullstacklabs.co>
2019-10-02 12:33:30 -0500

    Details screen updated to hold CocktailDetailsCard component

--- src/screens/Details/index.jsx
+++ src/screens/Details/index.jsx
@@ -2,6 +2,9 @@ import React, { PureComponent } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
+import CocktailDetailsCard from './components/CocktailDetailsCard';
+import { navigationShape } from '../../common/propTypes';
+
 import styles from './styles';
 import colors from '../../common/colors';
 
@@ -16,12 +19,19 @@ class Details extends PureComponent {
   });
 
   render() {
+    const { navigation } = this.props;
+    const { uri, measures, instructions } = navigation.state.params;
+
     return (
       <View style={styles.container}>
-        <Text>Hi there! I am Details</Text>
+        <CocktailDetailsCard uri={uri} measures={measures} instructions={instructions} />
       </View>
     );
   }
 }
+
+Details.propTypes = {
+  navigation: navigationShape.isRequired,
+};
 
 export default Details;
--- src/screens/Details/styles.js
+++ src/screens/Details/styles.js
@@ -5,6 +5,7 @@ import styles from '../../common/styles';
 
 export default StyleSheet.create({
   container: {
+    ...styles.paddingVertical,
     ...styles.paddingHorizontal,
     backgroundColor: colors.background,
     flex: 1,
